8986829d75dd7f923c93ee3b923863e9ad912bbc
KolesnichenkoTM <151588820+KolesnichenkoTM@users.noreply.github.com>
2023-12-26 10:10:00 +1000

    Add files via upload

---
+++ PerimeterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kolesnichenko;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolesnichenko.Tests
+{
+    [TestClass()]
+    public class PerimeterTests
+    {
+        [TestMethod()]
+        public void RectangularTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
